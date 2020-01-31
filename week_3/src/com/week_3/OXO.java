89d0cba457f761673024678873104d495abf75fe
nonac <nonacyang@gmail.com>
2020-01-31 10:48:28 +0000

    new file:   week_3/.idea/.gitignore 	new file:   week_3/.idea/description.html 	new file:   week_3/.idea/encodings.xml 	new file:   week_3/.idea/misc.xml 	new file:   week_3/.idea/modules.xml 	new file:   week_3/.idea/project-template.xml 	new file:   week_3/.idea/vcs.xml 	new file:   week_3/src/com/week_3/OXO.java 	new file:   week_3/src/com/week_3/clock.java 	new file:   week_3/week_3.iml

---
+++ week_3/src/com/week_3/OXO.java
@@ -0,0 +1,355 @@
+import java.io.*;
+
+enum Type {SPACE,O,X}
+
+public class OXO {
+    private Board board;
+    public static void main(String[] args) {
+        OXO oxo=new OXO();
+        oxo.run(args);
+    }
+    void run(String[] args)
+    {
+
+        board = new Board();
+        Dispaly dispaly =new Dispaly();
+        dispaly.begin();
+        if(!dispaly.isPVP()) {
+            board.changeToPVE();
+            if(!dispaly.isFirst()) {
+                board.changePlayerfirst();
+            }
+        }
+        dispaly.printout(board.show());
+
+        play(board,dispaly);
+
+        if(board.isWin()) {
+            dispaly.winEnding(board.showWinner());
+        }else if(board.isDraw()) {
+            dispaly.drawEnding();
+        }
+    }
+    void play(Board board,Dispaly dispaly)
+    {
+        String next;
+        if(board.showPlayMode()) {
+            Machine machine=new Machine();
+        }
+        do {
+            next=null;
+            if((!board.showPlayMode())||((board.showPlayMode())&&
+                    (((board.showPlayerfirst())&&(board.showTurn()))||
+                            (!board.showPlayerfirst())&&(!board.showTurn())))) {
+                do {
+                    if (!board.isValid(next)) {
+                        dispaly.errorInput();
+                    }
+                    next = dispaly.turnbegin(board.showTurn());
+                } while (!board.isValid(next));
+            }
+
+            board.move(next);
+            dispaly.printout(board.show());
+            board.nextTurn();
+        } while ((!board.isWin())&&(!board.isDraw()));
+    }
+
+}
+
+class Dispaly
+{
+
+    void begin()
+    {
+        System.out.println("Let us begin the game!");
+    }
+    boolean isPVP()
+    {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        do {
+            System.out.println("You want a PVP or PVE? (y for PVP, n for PVE): ");
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                throw new Error(e);
+            }
+        }while (!isPVPInputValid(str));
+
+        return str.charAt(0) == 'y';
+    }
+
+    boolean isFirst()
+    {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        do {
+            System.out.println("You want first? (y for first, n for second): ");
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                throw new Error(e);
+            }
+        }while (!isPVPInputValid(str));
+        return str.charAt(0) == 'y';
+    }
+
+    boolean isPVPInputValid(String str)
+    {
+        if(str.length()!=1) return false;
+        return (str.charAt(0) == 'y') || (str.charAt(0) == 'n');
+    }
+    void printout(Type[][] board)
+    {
+        int i,j;
+        String row0,col0;
+        row0 ="  A B C ";
+        System.out.println(row0);
+        for(i=0;i<3;i++)
+        {
+            col0=(i+1)+" ";
+            System.out.print(col0);
+            for (j=0;j<3;j++)
+            {
+                switch (board[i][j]){
+                    case SPACE:System.out.print(". ");break;
+                    case O:System.out.print("0 ");break;
+                    case X:System.out.print("X ");break;
+                }
+            }
+            System.out.println();
+        }
+    }
+    String turnbegin(boolean playerO){
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String player=playerO?"Player O":"Player X";
+        System.out.println("Now is "+player+"'s turn.");
+        System.out.print(player+"'s move:");
+
+        try{
+            str = br.readLine();
+        }catch (Exception e){ throw new Error(e); }
+
+        while (!isValid(str))
+        {
+            System.out.println("It is a un valid place. Enter in a new one.");
+            try{
+                str=br.readLine();
+            }catch (Exception e){ throw new Error(e); }
+        }
+        return str;
+    }
+    boolean isValid(String str)
+    {
+        if(str.length()!=2) return false;
+        if((str.charAt(0) < 'A') || (str.charAt(0) > ('A' + 2))) return false;
+        if((str.charAt(1) < '1') || (str.charAt(1) > ('1' + 2))) return false;
+        return true;
+    }
+    void errorInput()
+    {
+        System.out.println("It is a un valid place. Enter in a new one.");
+    }
+    void winEnding(Type winner)
+    {
+        String player=winner==Type.O?"Player O":"Player X";
+        System.out.println("Congratulations！"+player+" win the game!");
+    }
+    void drawEnding()
+    {
+        System.out.println("This is a draw!");
+    }
+}
+
+class Board
+{
+    private boolean playerO=true;
+    private Type[][] board = new Type[3][3];
+    private Type winner;
+    private boolean PVE = false;
+    private boolean playerFirst = true;
+    Board()
+    {
+        int i,j;
+        for(i=0;i<3;i++)
+        {
+            for (j=0;j<3;j++)
+            {
+                board[i][j]=Type.SPACE;
+            }
+        }
+    }
+    Type[][] show()
+    {
+        return board;
+    }
+    Type showWinner()
+    {
+        return winner;
+    }
+    void nextTurn()
+    {
+        playerO=!playerO;
+    }
+    boolean showTurn()
+    {
+        return playerO;
+    }
+    boolean isValid(String next)
+    {
+        if (next==null)return true;
+        return board[next.charAt(1) - '1'][next.charAt(0) - 'A'] == Type.SPACE;
+    }
+    boolean rowWin()
+    {
+        int i,j;
+        for(i=0;i<3;i++) {
+            for(j=0;j<3;j++) {
+                if(board[i][j]!=Type.O) {
+                    break;
+                }
+                if(j==2) {
+                    winner=Type.O;
+                    return true;
+                }
+            }
+        }
+        for(i=0;i<3;i++) {
+            for(j=0;j<3;j++) {
+                if(board[i][j]!=Type.X) {
+                    break;
+                }
+                if(j==2) {
+                    winner=Type.X;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    boolean colWin()
+    {
+        int i,j;
+        for(i=0;i<3;i++) {
+            for(j=0;j<3;j++) {
+                if(board[j][i]!=Type.O) {
+                    break;
+                }
+                if(j==2) {
+                    winner=Type.O;
+                    return true;
+                }
+            }
+        }
+        for(i=0;i<3;i++) {
+            for(j=0;j<3;j++) {
+                if(board[j][i]!=Type.X) {
+                    break;
+                }
+                if(j==2) {
+                    winner=Type.X;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    boolean slashwin()
+    {
+        int i;
+        for(i=0;i<3;i++){
+            if(board[i][i]!=Type.O) {
+                break;
+            }
+            if(i==2) {
+                winner=Type.O;
+                return true;
+            }
+        }
+        for(i=0;i<3;i++){
+            if(board[i][i]!=Type.X) {
+                break;
+            }
+            if(i==2) {
+                winner=Type.X;
+                return true;
+            }
+        }
+        for(i=0;i<3;i++){
+            if(board[2-i][i]!=Type.X) {
+                break;
+            }
+            if(i==2) {
+                winner=Type.X;
+                return true;
+            }
+        }
+        for(i=0;i<3;i++){
+            if(board[2-i][i]!=Type.O) {
+                break;
+            }
+            if(i==2) {
+                winner=Type.O;
+                return true;
+            }
+        }
+        return false;
+    }
+    boolean isWin()
+    {
+        return rowWin()||colWin()||slashwin();
+
+    }
+    boolean isDraw()
+    {
+        int i,j;
+        if(!isWin())
+        {
+            for (i=0;i<3;i++){
+                for(j=0;j<3;j++)
+                {
+                    if(board[i][j]==Type.SPACE)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void move(String next)
+    {
+        board[next.charAt(1) - '1'][next.charAt(0) - 'A']=playerO?Type.O:Type.X;
+    }
+    void changeToPVE() {
+        PVE = true;
+    }
+    boolean showPlayMode() {
+        return PVE;
+    }
+    void changePlayerfirst() {
+        playerFirst =false;
+    }
+    boolean showPlayerfirst() {
+        return playerFirst;
+    }
+}
+
+class Machine
+{
+    int random()
+    {
+        final long l = System.currentTimeMillis();
+        return (int)(l % 3);
+    }
+    String easyPVE()
+    {
+        char a = (char)((int)'A'+random());
+        char b = (char)((int)'1'+random());
+        return ""+a+b;
+    }
+}
