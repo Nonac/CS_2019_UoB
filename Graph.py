c3043e265451a331b2c79f493c6e6447c36ccff2
Nonac <nonacyang@gmail.com>
2020-08-27 14:03:28 +0800

    C5

--- Graph.py
+++ Graph.py
@@ -304,5 +304,5 @@ class Graph:
                         if group[i] == 1:
                             cnt += 1
             local.append(cnt)
-            print(local,edges)
+        print(local,edges)
         return local
